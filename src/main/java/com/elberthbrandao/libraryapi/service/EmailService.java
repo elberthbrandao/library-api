84724c072327869597445de5fe09e8fdfe045797
elberthbrandao <zelltr@hotmail.com>
2021-10-19 14:27:52 -0300

    Implementando o serviço de agendamento de tarefas

---
+++ src/main/java/com/elberthbrandao/libraryapi/service/EmailService.java
@@ -0,0 +1,7 @@
+package com.elberthbrandao.libraryapi.service;
+
+import java.util.List;
+
+public interface EmailService {
+    void sendMails(List<String> mailsList);
+}
---
+++ src/main/java/com/elberthbrandao/libraryapi/service/ScheduleService.java
@@ -0,0 +1,32 @@
+package com.elberthbrandao.libraryapi.service;
+
+import com.elberthbrandao.libraryapi.model.entity.Loan;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private static final String CRON_LATE_LOANS = "0 0 0 1/1 * ?";
+
+    @Value("${application.mail.lateloans.message}")
+    private String message;
+
+    private final LoanService loanService;
+    private final EmailService emailService;
+
+    @Scheduled(cron = CRON_LATE_LOANS)
+    public void sendEmailToLateLoans() {
+        List<Loan> allLateLoans = loanService.getAllLateLoans();
+        List<String> mailsList = allLateLoans.stream()
+                .map(Loan::getCustomerEmail)
+                .collect(Collectors.toList());
+
+        emailService.sendMails(mailsList);
+    }
+}
